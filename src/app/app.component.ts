5d804b0f9b8314f5eb1e8b7fae40fa292612b9b7
Chi Khang <khangfat2001@gmail.com>
2022-08-03 08:59:40 +0700

    MyCalcullator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mycalculator';
+  toshow = '0'
+  currvalue = ''
+  onclickinput(value:string){
+    this.currvalue = this.currvalue + value
+    this.toshow = this.currvalue
+  }
+  back(){
+    this.currvalue = this.currvalue.slice(0, -1)
+    this.toshow = this.currvalue
+    if(this.toshow==''){
+      this.toshow = '0'
+    }
+    
+  }
+  clear(){
+    this.currvalue = '';
+    this.toshow = '0'
+  }
+  equals(){
+    this.toshow = eval(this.currvalue)
+    this.currvalue = this.toshow
+  }
 }
